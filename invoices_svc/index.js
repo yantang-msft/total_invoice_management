81f6fc7a0d9cdfb301d64850eebfab734e87a02c
Karol Zadora-Przylecki <karolz@microsoft.com>
2018-06-01 15:29:46 -0700

    Add logging to invoice_svc

--- invoices_svc/index.js
+++ invoices_svc/index.js
@@ -1,16 +1,34 @@
 const express = require("express")
 const request = require("request-promise")
+const winston = require("winston")
 
 const app = express()
+
+var logger = new winston.Logger({
+  transports: [
+    new winston.transports.Console({
+      level: 'debug',
+      timestamp: true,
+      json: true,
+      stringify: true,
+      stderrLevels: [] // Output everything to stdout
+    })
+  ]
+});
+logger.emitErrs = false; // Do not emit error events from logger infrastructure
 
 const addExpectedDate = async invoice => {
   try {
     const { expectedDate } = await request(`${process.env.EXPECTED_DATE_URI}/api/expected-date/${invoice.id}`, {
       json: true
     })
+
     return Object.assign({}, invoice, { expectedDate })
-  } catch (e) {
-    console.log(`failed to add expected date ${e}`)
+  } catch (error) {
+    logger.warn('Failed to add expected date', {
+      error: error
+    });
+
     return invoice
   }
 }
@@ -18,6 +36,17 @@ const addExpectedDate = async invoice => {
 app.get("/api/invoices/:id", async (req, res, next) => {
   try {
     const id = parseInt(req.params.id)
+    if (isNaN(id)) {
+      logger.debug('Request failure', {
+        status: '400 Bad Request',
+        invoice_id: req.params.id,
+        details: 'Invoice id is invalid'
+      })
+
+      res.status(400).send({error: 'Invoice id is invalid'})
+      return
+    }
+
     const invoice = await addExpectedDate({
       id: id,
       ref: `INV-${id}`,
@@ -25,8 +54,21 @@ app.get("/api/invoices/:id", async (req, res, next) => {
       balance: (id * 100) - 10,
       ccy: "GBP"
     })
+
+    logger.debug('Request success', {
+      status: '200 OK',
+      invoice_id: req.params.id,
+      invoice: invoice
+    })
+
     res.json(invoice)
   } catch (error) {
+    logger.warn('Unexpected error occurred while processing request for invoice', {
+      status: '500 Internal Server Error',
+      invoice_id: req.params.id,
+      error: error
+    })
+
     next(error)
   }
 })
@@ -34,5 +76,5 @@ app.get("/api/invoices/:id", async (req, res, next) => {
 const port = process.env.PORT || 8080
 
 app.listen(port, () => {
-  console.log(`invoices_svc listening on ${port}`)
+  logger.info(`invoices_svc listening on ${port}`, {port: port})
 })

bef0fd1e07c80059859e9678e62614e16315aa2d
Karol Zadora-Przylecki <karolz@microsoft.com>
2018-06-14 10:28:43 -0700

    Update entry svc to actually proxy the incoming requests

--- entry_svc/index.js
+++ entry_svc/index.js
@@ -16,5 +16,17 @@ else {
     proxy.on('proxyReq', (clientReq, incomingMsg, response, options) => {
         // Disregard Request-Id even if present on client request 
         // (we do not trust external clients to set request IDs properly)
+        let activityId = new ActivityId();
+        clientReq.setHeader("Request-Id", activityId.id);
     });
+
+    const server = http.createServer(function(req, res) {
+        proxy.web(req, res, {
+            target: ambassadorUrl
+        });
+    });
+
+    const port = process.env.PORT || 8080;
+    logger.info(`entry_svc listening on ${port}`, {port: port});
+    server.listen(port);
 }
